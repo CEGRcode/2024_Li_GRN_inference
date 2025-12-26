68ccdf08faf76784d75ccb75227831292eea2932
Ruihao Li <42450996+UCM-RuihaoLi@users.noreply.github.com>
2025-12-26 18:01:20 -0500

    Update 1_Preprocessing_dicrete_gene_expression_state_identification.sh

--- IAGREE/1_Preprocessing_dicrete_gene_expression_state_identification.sh
+++ IAGREE/1_Preprocessing_dicrete_gene_expression_state_identification.sh
@@ -2,10 +2,11 @@
 # ---------------------- Preprocessing data and discrete gene expression state identification -----------
 # ---------------------- rl884@cornell.edu, 25/12/16 ----------------------------------------------------
 
+mkdir -p ./result/GMM_figures/AIC
 
 python GRN_input_acquisition.py \
   -p 0.01 \
-  -g ./data/All_gene_ID.txt \
+  -g ./data/ssTFs_MATa_Spots_76.txt \
   -r ./data/GRN_Sc_TMM_normalized_CPM.txt \
   -t ./data/Rossi_Ruihao_TF_DNA_union_motif_based.json \
   -c ./data/PPI_network_Cutoff_0_STRING_overlapping_motif_sites_0_2025_union.json \

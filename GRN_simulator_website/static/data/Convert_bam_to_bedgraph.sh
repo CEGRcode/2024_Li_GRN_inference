d18def59b3ad7e2adac40da4b77d2b379c460a60
Ruihao Li <42450996+UCM-RuihaoLi@users.noreply.github.com>
2026-04-08 11:28:49 -0400

    Update Convert_bam_to_bedgraph.sh

--- GRN_simulator_website/static/data/Convert_bam_to_bedgraph.sh
+++ GRN_simulator_website/static/data/Convert_bam_to_bedgraph.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# ---------------------- MAKE BEDGRAPH FOR EACH BAM ----------------------
-# ---------------------- rl884@cornell.edu, 24/08/29 ---------------------
-
 set -e
 
 module load PPI
@@ -20,10 +17,10 @@ for folder in "$INPUT_ROOT"/*_YEP/; do
     bam="${folder}${ID}_filtered_sorted.bam"
     [ -e "$bam" ] || continue
 
-    java -jar ./static/data/ScriptManager-v0.14.jar \
+    java -jar "$INPUT_ROOT/ScriptManager-v0.14.jar" \
         bam-format-converter bam-to-bedgraph \
         -o "${folder}${ID}" \
         "$bam"
 
-    mv "${folder}"*.bedGraph "$OUTDIR"/
+    mv "${folder}${ID}.bedGraph" "$OUTDIR"/
 done

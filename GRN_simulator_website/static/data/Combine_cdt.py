6b3b16116b831b96d94ef3cd78b911349d0de66c
Ruihao Li <42450996+UCM-RuihaoLi@users.noreply.github.com>
2026-04-08 11:17:15 -0400

    Update Combine_cdt.py

--- GRN_simulator_website/static/data/Combine_cdt.py
+++ GRN_simulator_website/static/data/Combine_cdt.py
@@ -1,49 +1,42 @@
 import os
+import sys
 import h5py
 import numpy as np
 import zipfile
 import shutil
 
-for filename in os.listdir('./'):
+input_dir = sys.argv[1] if len(sys.argv) > 1 else "./"
+output_dir = sys.argv[2] if len(sys.argv) > 2 else "./"
+
+for filename in os.listdir(input_dir):
     if filename.endswith('.zip'):
-        # Define the full path to the zip file
-        zip_file_path = os.path.join('./', filename)
-        
-        # Define the extraction directory
-        extract_dir = './SCALE_cdt/'
-        
-        # Create the extraction directory if it does not exist
-        if not os.path.exists(extract_dir):
-            os.makedirs(extract_dir)
-        
-        # Unzip the file
+        zip_file_path = os.path.join(input_dir, filename)
+        extract_dir = os.path.join(output_dir, 'SCALE_cdt')
+
+        if os.path.exists(extract_dir):
+            shutil.rmtree(extract_dir)
+        os.makedirs(extract_dir)
+
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
             zip_ref.extractall(extract_dir)
 
-        # Create a new HDF5 file
-        output_h5 = filename.split('_')[0] + '_BED.h5'
+        output_h5 = os.path.join(output_dir, filename.split('_')[0] + '_BED.h5')
         with h5py.File(output_h5, 'w') as h5file:
-            # Loop through each .cdt file in the directory
-            for cdt_file in os.listdir('./SCALE_cdt/SCALE_cdt/'):
+            for cdt_file in os.listdir(os.path.join(extract_dir, 'SCALE_cdt')):
                 if cdt_file.endswith('.cdt'):
-                    file_path = os.path.join('./SCALE_cdt/SCALE_cdt/', cdt_file)
-                    # Read the content of the .cdt file
+                    file_path = os.path.join(extract_dir, 'SCALE_cdt', cdt_file)
                     with open(file_path, 'r') as file:
                         content = file.readlines()
-                        
-                        # Create a dataset in the HDF5 file for this .cdt file
-                        dataset_name = os.path.splitext(cdt_file)[0]
-                        
-                        # Store the content as a dataset in the HDF5 file
-                        h5file.create_dataset(dataset_name, data=np.array(content, dtype='S'), compression="gzip")
 
-                        # Add metadata to identify the source file
-                        h5file[dataset_name].attrs['source_file'] = cdt_file
+                    dataset_name = os.path.splitext(cdt_file)[0]
+                    h5file.create_dataset(
+                        dataset_name,
+                        data=np.array(content, dtype='S'),
+                        compression="gzip"
+                    )
+                    h5file[dataset_name].attrs['source_file'] = cdt_file
 
         print(f"All .cdt files have been concatenated into {output_h5}.")
-    else:
-        pass
-    if os.path.isdir('./SCALE_cdt/'):
-        shutil.rmtree('./SCALE_cdt/')
-    else:
-        pass
+
+        if os.path.isdir(extract_dir):
+            shutil.rmtree(extract_dir)

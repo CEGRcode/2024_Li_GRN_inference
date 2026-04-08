4652776094d592b786afa10fedcdf2a40e94a797
Ruihao Li <42450996+UCM-RuihaoLi@users.noreply.github.com>
2026-04-08 11:41:25 -0400

    Update Full_dataset_process.sh

--- GRN_simulator_website/Full_dataset_process.sh
+++ GRN_simulator_website/Full_dataset_process.sh
@@ -6,5 +6,8 @@ module load PPI
 INPUT_DIR="../Protein_protein_interaction_network"
 
 bash ./static/data/Convert_bam_to_bedgraph.sh "$INPUT_DIR"
-python ./static/data/Combine_cdt.py "$INPUT_DIR" ./static/data
+python ./static/data/Combine_cdt.py "$INPUT_DIR/SCALE_cdt" ./static/data
 python ./static/data/Combine_h5.py ./static/data
+
+# clean intermediate files
+rm -f ./static/data/*_BED.h5

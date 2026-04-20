0186a74a6f33d335696bc2833bcaa65cb10b1cd8
Ruihao Li <42450996+UCM-RuihaoLi@users.noreply.github.com>
2026-04-20 12:26:08 -0400

    Update 5_GRN_performance_evaluation.sh

--- SETIA/5_GRN_performance_evaluation.sh
+++ SETIA/5_GRN_performance_evaluation.sh
@@ -8,7 +8,7 @@
 # ------------------------------------------------------------------
 
 python GRN_Dynamic_Simulator_Combinatorial_Local_multistate_2025.py \
-  -j ./result/GRN_filtered_full_Sc_GRN_final_raw.txt \
+  -j ./result/GRN_filtered_full_Sc_GRN_final.txt \
   -r ./data/GRN_ssTFs_Salmon_SteadyStates_2025_discrete.txt \
   -l ./data/GRN_ssTFs_Sc_gene_length.txt \
   -t ./data/GRN_ssTFs_Sc_promoter_strength.txt \
